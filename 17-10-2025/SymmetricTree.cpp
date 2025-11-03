358f58107ba54be1590771c02a001b05fbc3900a
Ayush Ojha <ayushojha4373@gmail.com>
2025-11-03 21:11:35 +0530

    Create SymmetricTree.cpp

---
+++ 17-10-2025/SymmetricTree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isMirror(TreeNode* a, TreeNode* b) {
+        if (!a && !b) 
+        return true;
+
+        if (!a || !b)
+        return false;
+        
+        return (a->val == b->val) && isMirror(a->left, b->right) && isMirror(a->right, b->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) 
+        return true;
+
+        return isMirror(root->left, root->right);
+    }
+};
