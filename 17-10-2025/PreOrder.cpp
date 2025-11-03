315578c87eb58a233d52eae10b80fe506cb6b502
Ayush Ojha <ayushojha4373@gmail.com>
2025-11-03 21:09:54 +0530

    Create PreOrder.cpp

---
+++ 17-10-2025/PreOrder.cpp
@@ -0,0 +1,18 @@
+void pre(TreeNode* root ,vector<int>&ans ){
+    if(root==NULL)
+        return ;
+    
+    ans.push_back(root->val);
+    pre(root->left , ans);
+    pre(root->right , ans);
+    
+    
+}
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        pre(root,ans);
+        return ans;
+    }
+};
